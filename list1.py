59a6df9cb3fba76bd68d1b7de156330c6d059e61
mummy11 <60389197+mummy11@users.noreply.github.com>
2020-01-30 15:36:57 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,26 @@
+# Python program to demonstrate 
+# Creation of List 
+
+# Creating a List 
+List = [] 
+print("Intial blank List: ") 
+print(List) 
+
+# Creating a List with 
+# the use of a String 
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+
+# Creating a List with 
+# the use of multiple values 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0]) 
+print(List[2]) 
+
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Geeks', 'For'] , ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List) 
